b92fab4d387a53d63a92c984a84815453296336a
Joshua Randle <randlejo@oregonstate.edu>
2023-04-26 21:56:55 -0500

    Added data class for concert info Completed search function Completed adapter Modified mainactivity Features for this week milestone complete

--- app/src/main/java/com/example/concertfinder/ConcertAdapter.kt
+++ app/src/main/java/com/example/concertfinder/ConcertAdapter.kt
@@ -3,20 +3,24 @@ package com.example.concertfinder
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class ConcertAdapter(private val concertInfoLists: MutableList<List<String>>) : RecyclerView.Adapter<ConcertAdapter.ViewHolder>() {
+class ConcertAdapter(private val concertInfoLists: List<ConcertData>) : RecyclerView.Adapter<ConcertAdapter.ViewHolder>() {
     inner class ViewHolder(view:View) : RecyclerView.ViewHolder(view) {
         val concert_Name: TextView
         val concert_Location: TextView
         val concert_Date: TextView
-        //val concert_Image: ImageView
+        val concert_Image: ImageView
+
         init {
             concert_Name = view.findViewById(R.id.concertName)
             concert_Location = view.findViewById(R.id.concertLocation)
             concert_Date = view.findViewById(R.id.concertDate)
-            //concert_Image = view.findViewById(R.id.concertImage)
+            concert_Image = view.findViewById(R.id.concertImage)
+
         }
     }
 
@@ -27,12 +31,22 @@ class ConcertAdapter(private val concertInfoLists: MutableList<List<String>>) : 
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val concert: ConcertData = concertInfoLists[position]
+
+        holder.concert_Name.text = concert.concertName
+        holder.concert_Location.text = concert.concertLocation
+        holder.concert_Date.text = concert.concertDate
+        Glide.with(holder.itemView)
+            .load(concert.concertImage)
+            .centerCrop()
+            .into(holder.concert_Image)
+
     }
+
 
     override fun getItemCount() = concertInfoLists.size
 
-    //override fun onBindViewHolder(holder: Viewholder, position: Int){
+
 
 
 }
---
+++ app/src/main/java/com/example/concertfinder/ConcertData.kt
@@ -0,0 +1,10 @@
+package com.example.concertfinder
+
+import androidx.annotation.DrawableRes
+
+data class ConcertData(
+    val concertName: String,
+    val concertLocation: String,
+    val concertDate: String,
+    var concertImage: String
+)
